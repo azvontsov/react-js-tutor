69fbe6114d0b9be6c66dc20f9aa376ebb28eb805
Anton <anton@Polinas-MacBook-Air.local>
2022-01-11 15:35:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+
+import "./styles/App.css";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript", body: "Description" },
+    { id: 2, title: "JavaScript 2", body: "Description" },
+    { id: 3, title: "JavaScript 3", body: "Description" },
+  ]);
+
+  const [post, setPost] = useState({ title: "", body: "" });
+  const [body, setBody] = useState("");
+
+  // const bodyInputRef = useRef();
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+
+    setPosts([...posts, { ...post, id: Date.now() }]);
+    setPost({ title: "", body: "" });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        {/*Controlled Component */}
+        <MyInput
+          value={post.title}
+          onChange={(e) => setPost({ ...post, title: e.target.value })}
+          type="text"
+          placeholder="Post Name"
+        />
+
+        {/* Uncontrolled Component */}
+        <MyInput
+          // ref={bodyInputRef}
+          value={post.body}
+          onChange={(e) => setPost({ ...post, body: e.target.value })}
+          type="text"
+          placeholder="Post Description"
+        />
+        <MyButton onClick={addNewPost}>Create Post</MyButton>
+      </form>
+      <PostList posts={posts} title="Index of JS Posts" />
     </div>
   );
 }
